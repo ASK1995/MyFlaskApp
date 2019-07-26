526b6fa3f3fd089c94c5ff48bcecdc62ee3c3b60
Santosh Kumar <a.santoshkumar1995@gmail.com>
2019-07-26 13:24:11 -0700

    first commit

---
+++ myflaskapp/data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'Lorem',
+			'author' : 'Santosh',
+			'create_date' : '07-26-2019'
+		},
+		{
+			'id' : 2,
+			'title' : 'Article Two',
+			'body' : 'Minds',
+			'author' : 'Jane Doe',
+			'create_date' : '07-25-2019'
+		},
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'Grey',
+			'author' : 'John Doe',
+			'create_date' : '07-27-2019'
+		},
+		
+			
+	]
+	return articles
